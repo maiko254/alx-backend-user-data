e92750f60b3b1173ab889af2f001185b909df082
maiko254 <bonyomichael406.com>
2024-08-07 14:53:49 +0300

    feat: Added method to class Auth that retrieves cookie value from a request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Auth class for Basic authentication"""
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -28,3 +29,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """doc doc doc"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = getenv("SESSION_NAME", "_my_session_id")
+        return request.cookies.get(session_name)
