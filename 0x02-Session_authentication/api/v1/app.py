0f804730cba3bf4a1af153129505fdb5cf65c6e5
maiko254 <bonyomichael406.com>
2024-08-07 23:39:47 +0300

    Correction

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,7 +56,7 @@ def before_request():
         return
     if auth.authorization_header(request) is None and\
             auth.session_cookie(request) is None:
-        return None, abort(401)
+        abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
