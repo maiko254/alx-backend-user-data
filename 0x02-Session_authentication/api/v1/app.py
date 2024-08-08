9bef42c1238de6384bc8ae2f267a54c0a89900b0
maiko254 <bonyomichael406.com>
2024-08-08 22:55:37 +0300

    Correction: Fixed authentication process of user before processing a request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,7 +47,7 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request():
-    """fiiltering routes"""
+    """Authenticating user before processing a request"""
     if auth is None:
         return
     ex = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
@@ -57,11 +57,9 @@ def before_request():
     if auth.authorization_header(request) is None and\
             auth.session_cookie(request) is None:
         abort(401)
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
     user = auth.current_user(request)
+    if user is None:
+        abort(403)
     request.current_user = user
 
 
