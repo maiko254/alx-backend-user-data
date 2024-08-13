f7958378dada7748e549963bb12938320b292cc2
maiko254 <bonyomichael406.com>
2024-08-13 13:41:35 +0300

    feat: Added class Auth wiith method register user to add a new user with an email and hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 """Module to authenticate user"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with the given email and password """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pw = _hash_password(password)
+            return self._db.add_user(email, hashed_pw)
 
 
 def _hash_password(password: str) -> str:
