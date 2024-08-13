550e85e717aed8138eafb47ce2783061d6ae0e97
maiko254 <bonyomichael406.com>
2024-08-13 23:06:33 +0300

    feat: Added method that gets user fro m a session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,6 +43,14 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session_id """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> str:
     """Hash the given password
