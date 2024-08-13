37e2c25f62a8801f0a8826eef82d98e011b41e9e
maiko254 <bonyomichael406.com>
2024-08-13 15:58:59 +0300

    feat: Added method to create a new session for a given user with a valid email

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,6 +33,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """Create a session for the given email """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> str:
     """Hash the given password
