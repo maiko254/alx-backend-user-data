5bf260dc7f3ab33dee58b7364c1983106ffea108
maiko254 <bonyomichael406.com>
2024-08-13 23:31:10 +0300

    feat: Added method to destroy a session associated with a given user_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,6 +51,11 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy the session associated with the given user_id """
+        self._db.update_user(user_id, session_id=None)
+        return None
+
 
 def _hash_password(password: str) -> str:
     """Hash the given password
