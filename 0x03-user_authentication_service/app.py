0a4fa0ef924fd82eb05746fc2a588cedcbec5a57
maiko254 <bonyomichael406.com>
2024-08-14 10:35:53 +0300

    feat: Added route /sessions to destroy a session linked to a user_id or logout user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module implementing a basic Flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -24,6 +24,33 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def sessions() -> str:
+    """Create a session for the user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            return jsonify({"email": email, "message": "logged in"})
+        else:
+            abort(401)
+    else:
+        abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def delete_session() -> str:
+    """Destroy a session for the user"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
