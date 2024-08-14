5de08b3cf66473146fd9ebd7219faee01e5db626
maiko254 <bonyomichael406.com>
2024-08-14 13:26:32 +0300

    feat: Added Auth.update_password method to update ae user password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,6 +66,16 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the password of the user """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pw = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pw)
+            self._db.update_user(user.id, reset_token=None)
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> str:
     """Hash the given password
